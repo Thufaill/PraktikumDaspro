a8c6be9a3cca30f5b1af05467e41c400423cce79
Thufaill <javierrthufaill@gmail.com>
2025-11-03 18:17:42 +0700

    Modifikasi Percobaan 2

--- jobsheet8/Square18.java
+++ jobsheet8/Square18.java
@@ -10,7 +10,7 @@ public class Square18 {
         int n = sc.nextInt();
 
         for (int iOuter = 1; iOuter <= n; iOuter++) {
-            for (int i = 1; i <= n; i++) {
+            for (int i = 0; i <= n; i++) {
                 System.out.print("* ");
             }
             System.out.println();
