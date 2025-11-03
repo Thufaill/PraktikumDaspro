6497c2fdba4399de8b3b39e75c1721af049908b5
Thufaill <javierrthufaill@gmail.com>
2025-11-03 18:38:21 +0700

    Modifikasi Percobaan 3

--- jobsheet8/Triangle18.java
+++ jobsheet8/Triangle18.java
@@ -14,6 +14,7 @@ public class Triangle18 {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
     sc.close();
