426599c675b263966fc2dcb9bc74cedad4756c3f
Thufaill <javierrthufaill@gmail.com>
2025-11-03 20:44:10 +0700

    Tugas 3

---
+++ jobsheet8/Tugas3CafeSenja18.java
@@ -0,0 +1,44 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class Tugas3CafeSenja18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int totalPelanggan = 0;
+        int totalItem = 0;
+
+        System.out.print("Jumlah cabang kafe: ");
+        int jumlahCabang = sc.nextInt();
+
+        System.out.println("\n=== Input Penjualan Per Cabang ===");
+
+        for (int i = 1; i <= jumlahCabang; i++) {
+            System.out.println("\n-- Cabang " + i + " --");
+            System.out.print("Jumlah pelanggan: ");
+            int jumlahPelanggan = sc.nextInt();
+
+            int totalItemPerCabang = 0;
+
+            for (int j = 1; j <= jumlahPelanggan; j++) {
+                System.out.print("- Pelanggan " + j + " memesan berapa item? ");
+                int jumlahItem = sc.nextInt();
+                totalItemPerCabang += jumlahItem;
+            }
+
+            System.out.println("Cabang " + i + ": ");
+            System.out.println("- Pelanggan: " + jumlahPelanggan + " orang");
+            System.out.println("- Item terjual: " + totalItemPerCabang + " item");
+
+            totalPelanggan += jumlahPelanggan;
+            totalItem += totalItemPerCabang;
+        }
+
+        System.out.println("\nTotal seluruh Cabang:");
+        System.out.println("- Pelanggan: " + totalPelanggan + " orang");
+        System.out.println("- Item terjual: " + totalItem + " item");
+
+        sc.close();
+    }
+}
