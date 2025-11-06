2e542b3b8b4110f4e110eec890f149a282896d88
Thufaill <javierrthufaill@gmail.com>
2025-11-06 19:08:13 +0700

    Modifikasi Percobaan 3

--- jobsheet9/ArrayRataNilai18.java
+++ jobsheet9/ArrayRataNilai18.java
@@ -6,15 +6,15 @@ public class ArrayRataNilai18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double total = 0, rata2;
-        int siswaLulus = 0;
+        int mhsLulus = 0;
         System.out.print("Masukkan jumlah siswa : ");
-        int jmlSiswa = sc.nextInt();
-        int nilaiMhs[] = new int[jmlSiswa];
+        int jmlMhs = sc.nextInt();
+        int nilaiMhs[] = new int[jmlMhs];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
-            siswaLulus = (nilaiMhs[i] > 70) ? ++siswaLulus : siswaLulus;
+            mhsLulus = (nilaiMhs[i] > 70) ? ++mhsLulus : mhsLulus;
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
@@ -23,7 +23,7 @@ public class ArrayRataNilai18 {
 
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata rata nilai = " + rata2);
-        System.out.println("Jumlah Mahasiswa lulus = " + siswaLulus);
+        System.out.println("Jumlah Mahasiswa lulus = " + mhsLulus);
         sc.close();
     }
 }
