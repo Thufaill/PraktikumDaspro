d6746b256ca9e280316885d3219f3d06cc7f4adc
Thufaill <javierrthufaill@gmail.com>
2025-12-01 23:58:49 +0700

    Percobaan 2

--- jobsheet11/Kafe18.java
+++ jobsheet11/Kafe18.java
@@ -1,18 +1,24 @@
 package jobsheet11;
 
 public class Kafe18 {
-    public static void main(String[] args) {
-        Menu();
-    }
-    public static void Menu() {
-        System.out.println("===== MENU RESTO KAFE =====");
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member,dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        System.out.println("=== Menu Resto Kafe ===");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("================================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        System.out.println("========================");
+    }
+
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
