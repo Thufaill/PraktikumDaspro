e9c3d33c55067799974b1720ed6f9fefaf522dbd
Thufaill <javierrthufaill@gmail.com>
2025-12-02 00:49:11 +0700

    Percobaan 3

--- jobsheet11/Kafe18.java
+++ jobsheet11/Kafe18.java
@@ -1,5 +1,8 @@
 package jobsheet11;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Kafe18 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang " + namaPelanggan + "!");
@@ -26,7 +29,44 @@ public class Kafe18 {
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
 
+    public static int hitungTotalHarga18(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
+
     public static void main(String[] args) {
-        Menu("Budi", true, "DISKON30");
+        Scanner sc = new Scanner(System.in);
+        String kodeVoucher = "kosong";
+        boolean isMember;
+        String member[] = {"Budi","Vier","Iben"};
+
+        System.out.print("Masukkan nama : ");
+        String nama = sc.nextLine();
+        isMember = Arrays.stream(member).anyMatch(nama::equals);
+
+        System.out.println("================================");
+        System.out.println("Apakah memiliki kode promo ? (Y/N)");
+        System.out.print("=> ");
+
+        String isAnyVoucher = sc.nextLine();
+        if (isAnyVoucher.equalsIgnoreCase("Y")) {
+            System.out.print("Kode Voucher = ");
+            kodeVoucher = sc.nextLine();
+        }
+        System.out.println("================================");
+
+        Menu(nama, isMember, kodeVoucher);
+        System.out.println("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenuu = sc.nextInt();
+        System.out.println("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga18(pilihanMenuu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
+
+        sc.close();
     }
 }
